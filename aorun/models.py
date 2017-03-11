07355facf5ca5b1744e946ba0656a76bbdebd442
Ramon de Oliveira <mail@roliveira.net>
2017-03-10 22:11:10 -0300

    add model forward

--- aorun/models.py
+++ aorun/models.py
@@ -4,6 +4,17 @@ class Model(object):
 
     def __init__(self, *layers):
         self.layers = list(layers)
+        self.build()
+
+    def build(self):
+        for prev_layer, next_layer in zip(self.layers[:-1], self.layers[1:]):
+            next_layer.build(prev_layer.output_dim)
 
     def add(self, layer):
         self.layers.append(layer)
+
+    def forward(self, x):
+        y = self.layers[0].forward(x)
+        for layer in self.layers[1:]:
+            y = layer.forward(y)
+        return y
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,10 @@
 from .context import aorun
 
 from aorun.models import Model
-from aorun.layers import Dense, ProbabilisticDense, Relu
+from aorun.layers import Dense
+from aorun.layers import Relu
+
+import torch
 
 
 def test_model_constructor_empty():
@@ -16,13 +19,12 @@ def test_model_constructor_layers():
     model = Model(
         Dense(10),
         Relu(),
-        Dense(1),
-        ProbabilisticDense(20)
+        Dense(1)
     )
 
-    assert len(model.layers) == 4
+    assert len(model.layers) == 3
     assert type(model.layers[0]) == Dense
-    assert type(model.layers[3]) == ProbabilisticDense
+    assert type(model.layers[1]) == Relu
 
 
 def test_model_add_layers():
@@ -31,8 +33,21 @@ def test_model_add_layers():
     model.add(Dense(10))
     model.add(Relu())
     model.add(Dense(1))
-    model.add(ProbabilisticDense(20))
 
-    assert len(model.layers) == 4
+    assert len(model.layers) == 3
     assert type(model.layers[0]) == Dense
-    assert type(model.layers[3]) == ProbabilisticDense
+    assert type(model.layers[1]) == Relu
+
+
+def test_model_forward():
+
+    model = Model(
+        Dense(10, input_dim=4),
+        Dense(1),
+        Dense(20)
+    )
+
+    x = torch.Tensor(2, 4)
+    y = model.forward(x)
+
+    assert y.size() == (2, 20)
