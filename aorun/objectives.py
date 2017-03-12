3a336d0bfe987e503ebaad0875f754dfffa12083
Ramon de Oliveira <mail@roliveira.net>
2017-03-12 03:41:21 -0300

    add binary and categorical crossentropy objectives

--- aorun/objectives.py
+++ aorun/objectives.py
@@ -4,6 +4,17 @@ import torch
 def mean_squared_error(true, pred):
     return torch.mean((true - pred)**2)
 
+
+def binary_crossentropy(true, pred, eps=1e-9):
+    p1 = true * torch.log(pred + eps)
+    p2 = (1 - true) * torch.log(1 - pred + eps)
+    return torch.mean(-(p1 + p2))
+
+
+def categorical_crossentropy(true, pred, eps=1e-9):
+    return torch.mean(-torch.sum(true * torch.log(pred + eps), dim=1))
+
+
 # aliases short names
 mse = mean_squared_error
 
--- tests/test_objectives.py
+++ tests/test_objectives.py
@@ -3,6 +3,8 @@ from .context import aorun
 import torch
 from torch.autograd import Variable
 from aorun.objectives import mean_squared_error
+from aorun.objectives import binary_crossentropy
+from aorun.objectives import categorical_crossentropy
 
 
 def test_mse_variable():
@@ -22,3 +24,25 @@ def test_mse_tensor():
     loss = mean_squared_error(true, pred)
 
     assert loss == 1
+
+
+def test_binary_crossentropy():
+    true = torch.Tensor([1, 1, 1])
+
+    loss = binary_crossentropy(true, true)
+    assert loss == 0
+
+    pred = torch.Tensor([[0, 0, 0]])
+    loss = binary_crossentropy(true, pred)
+    assert loss > 1
+
+
+def test_categorical_crossentropy():
+    true = torch.Tensor([[1, 0], [0, 1], [1, 0]])
+
+    loss = categorical_crossentropy(true, true)
+    assert loss == 0
+
+    pred = torch.Tensor([[0, 1], [0, 1], [1, 0]])
+    loss = categorical_crossentropy(true, pred)
+    assert loss > 1
