57335fcb1486be453b8c6be347ec8a6555870701
Ramon Oliveira <ramon@roliveira.net>
2017-03-17 17:01:27 -0300

    clean regression example

--- examples/regression.py
+++ examples/regression.py
@@ -13,20 +13,19 @@ from aorun.layers import Activation
 from aorun.optimizers import SGD
 
 X, y = datasets.load_boston(return_X_y=True)
-X = X.astype('float32')
-y = y.astype('float32')
-X = StandardScaler().fit_transform(X)
+X = StandardScaler().fit_transform(X).astype('float32')
+y = StandardScaler().fit_transform(y).astype('float32')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 model = Model(
-    Dense(10, input_dim=X_train.shape[-1]),
+    Dense(100, input_dim=X_train.shape[-1]),
     Activation('relu'),
-    Dense(10),
+    Dense(100),
     Activation('relu'),
     Dense(1)
 )
 
-sgd = SGD(lr=0.005)
+sgd = SGD(lr=0.1)
 history = model.fit(X_train, y_train, loss='mse', optimizer=sgd, n_epochs=500)
 y_pred = model.forward(X_test)
 print('r2_score:', metrics.r2_score(y_test, y_pred))
