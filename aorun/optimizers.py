1e752029afe4b132cdce595acc8ac7c08ee14063
Ramon Oliveira <ramon@roliveira.net>
2017-03-21 16:43:55 -0300

    add sgd momentum and decay

--- aorun/optimizers.py
+++ aorun/optimizers.py
@@ -1,29 +1,40 @@
-from torch.optim import SGD as SGD_torch
+import torch
+from torch.autograd import Variable
+from torch.optim import Adam
 
 
 class Optimizer(object):
-    pass
-
-
-class SGD(Optimizer):
-
-    def __init__(self, params=None, lr=0.001, momentum=0.0, nesterov=False):
-        self.params = params
-        self.lr = lr
-        self.momentum = momentum
-        self.nesterov = nesterov
 
     def clear_gradients(self):
         if self.params:
             for p in self.params:
                 p.grad.data.zero_()
 
+
+class SGD(Optimizer):
+
+    def __init__(self, params=None, lr=0.001, decay=0.0, momentum=0.0):
+        self.params = params
+        self.lr = lr
+        self.decay = decay
+        self.momentum = momentum
+        self.updates = []
+
     def step(self, clear=True):
         if self.params is None:
             raise Exception('None parameters')
 
-        for p in self.params:
-            p.data.sub_(self.lr * p.grad.data)
+        if len(self.updates) == 0:
+            for p in self.params:
+                update = torch.zeros(p.size())
+                self.updates.append(update)
+
+        for i, (p, update) in enumerate(zip(self.params, self.updates)):
+            cur_update = self.momentum * update + self.lr * p.grad.data
+            p.data.sub_(cur_update)
+            self.updates[i] = cur_update
+
+        self.lr = max(1e-9, self.lr - self.decay)
 
         if clear:
             self.clear_gradients()
