2a1846808371f96235c78868159fa5a767484c1f
Ramon Oliveira <ramon@roliveira.net>
2017-03-21 16:44:41 -0300

    fix probabilistic classification example

--- examples/probabilistic_classification.py
+++ examples/probabilistic_classification.py
@@ -13,9 +13,8 @@ from aorun.layers import ProbabilisticDense
 from aorun.layers import Activation
 from aorun.optimizers import SGD
 from aorun.losses import variational_loss
-from aorun.losses import categorical_crossentropy
 
-X, y = datasets.load_iris(return_X_y=True)
+X, y = datasets.load_digits(return_X_y=True)
 X = X.astype('float32')
 y = np.eye(y.max() + 1)[y].astype('float32')
 X = StandardScaler().fit_transform(X)
@@ -23,20 +22,20 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 print(X_train.shape, y_train.shape)
 
 model = Model(
-    ProbabilisticDense(10, input_dim=X_train.shape[-1]),
+    ProbabilisticDense(200, input_dim=X_train.shape[-1]),
     Activation('relu'),
-    ProbabilisticDense(10),
+    ProbabilisticDense(200),
     Activation('relu'),
     ProbabilisticDense(y_test.shape[-1]),
     Activation('softmax')
 )
 
-sgd = SGD(lr=0.001)
-loss = functools.partial(variational_loss,
-                         model=model,
-                         log_likelihood=categorical_crossentropy)
-history = model.fit(X_train, y_train, n_epochs=500, loss=loss, optimizer=sgd)
+opt = SGD(lr=0.1, momentum=0.9)
+loss = variational_loss(model, 'categorical_crossentropy')
+history = model.fit(X_train, y_train, n_epochs=20, loss=loss, optimizer=opt)
 
 y_pred = model.forward(X_test)
 acc = metrics.accuracy_score(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+print('test samples:', len(y_test))
+print('classes:', len(y_test[0]))
 print('Accuracy:', acc)
