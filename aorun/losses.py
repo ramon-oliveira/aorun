d9a98e97c783bef902a3904fd43ce53b518274fb
Ramon Oliveira <ramon@roliveira.net>
2017-03-21 16:41:45 -0300

    add optimizion to bias

--- aorun/losses.py
+++ aorun/losses.py
@@ -1,4 +1,4 @@
-import numpy as np
+import math
 import torch
 from torch import Tensor
 from torch.autograd import Variable
@@ -30,34 +30,41 @@ def categorical_crossentropy(true, pred, eps=1e-9):
     return torch.mean(-torch.sum(true * torch.log(pred + eps), dim=1))
 
 
-def variational_loss(model, log_likelihood):
-    log_likelihood = get(log_likelihood)
+def variational_loss(model, negative_log_likelihood):
+    negative_log_likelihood = get(negative_log_likelihood)
     prior_ratio = 0.5
-    prior_mu = Variable(Tensor([0]))
-    prior_sigma1 = Variable(Tensor([2.0]))
-    prior_sigma2 = Variable(Tensor([0.05]))
+    prior_mu = Variable(Tensor([0.0]))
+    prior_sigma1 = Variable(Tensor([10.0]))
+    prior_sigma2 = Variable(Tensor([0.5]))
 
     def loss(true, pred):
         log_p = Variable(torch.Tensor([0.0]))
         log_q = Variable(torch.Tensor([0.0]))
         for layer in model.layers:
             if type(layer) is ProbabilisticDense:
-                # posterior
-                x = layer.W
-                mu = layer.W_mu
-                sigma = torch.log1p(torch.exp(layer.W_rho))
-                log_q += log_gaussian(x, mu, sigma).sum()
-
                 # prior
-                mu = prior_mu.expand_as(x)
-                sigma1 = prior_sigma1.expand_as(x)
-                sigma2 = prior_sigma2.expand_as(x)
-                p1 = prior_ratio * log_gaussian(x, mu, sigma1)
-                p2 = (1 - prior_ratio) * log_gaussian(x, mu, sigma2)
+                mu = prior_mu.expand_as(layer.W)
+                sigma1 = prior_sigma1.expand_as(layer.W)
+                sigma2 = prior_sigma2.expand_as(layer.W)
+                p1 = prior_ratio * log_gaussian(layer.W, mu, sigma1)
+                p2 = (1 - prior_ratio) * log_gaussian(layer.W, mu, sigma2)
                 log_p += torch.sum(p1 + p2)
 
-        ll = -log_likelihood(true, pred)
-        return (log_q - log_p) / model.batches - ll
+                mu = prior_mu.expand_as(layer.b)
+                sigma1 = prior_sigma1.expand_as(layer.b)
+                sigma2 = prior_sigma2.expand_as(layer.b)
+                p1 = prior_ratio * log_gaussian(layer.b, mu, sigma1)
+                p2 = (1 - prior_ratio) * log_gaussian(layer.b, mu, sigma2)
+                log_p += torch.sum(p1 + p2)
+
+                # posterior
+                sigma = torch.log1p(torch.exp(layer.W_rho))
+                log_q += log_gaussian(layer.W, layer.W_mu, sigma).sum()
+                sigma = torch.log1p(torch.exp(layer.b_rho))
+                log_q += log_gaussian(layer.b, layer.b_mu, sigma).sum()
+
+        ll = -negative_log_likelihood(true, pred)
+        return ((log_q - log_p) / model.batches - ll) / model.batch_size
     return loss
 
 # aliases short names
