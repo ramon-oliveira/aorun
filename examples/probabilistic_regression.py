f397c4de34d8622968c250d6e3715bd043634c61
Ramon Oliveira <ramon@roliveira.net>
2017-03-22 10:59:36 -0300

    fix probabilistic regression

--- examples/probabilistic_regression.py
+++ examples/probabilistic_regression.py
@@ -12,6 +12,7 @@ from aorun.models import Model
 from aorun.layers import ProbabilisticDense
 from aorun.layers import Activation
 from aorun.optimizers import SGD
+from aorun.losses import variational_loss
 
 X, y = datasets.load_boston(return_X_y=True)
 X = X.astype('float32')
@@ -26,8 +27,9 @@ model.add(ProbabilisticDense(2))
 model.add(Activation('relu'))
 model.add(ProbabilisticDense(1))
 
-sgd = SGD(lr=0.005)
-history = model.fit(X_train, y_train, loss='mse', optimizer=sgd, n_epochs=500)
+sgd = SGD(lr=0.1)
+loss = variational_loss(model, 'mean_squared_error')
+history = model.fit(X_train, y_train, loss=loss, optimizer=sgd, epochs=100)
 y_pred = model.forward(X_test)
 print('r2_score:', metrics.r2_score(y_test, y_pred))
 print('mean_absolute_error:', metrics.mean_absolute_error(y_test, y_pred))
